de2629d5fe11a2b59c111b46d39b6caa57963767
Anoop Kanippayyur <Anoop.Kanippayyur@gmail.com>
2016-10-09 16:16:24 -0700

    Combinations on the Function overloading.

--- src/main/java/com/app/algorithms/string/FunctionOverloading.java
+++ src/main/java/com/app/algorithms/string/FunctionOverloading.java
@@ -1,6 +1,10 @@
 package com.app.algorithms.string;
 
 /**
+ * There are two ways to overload the method in java
+ *      By changing number of arguments
+ *      By changing the data type
+ *
  * Function overloading when passed with Null, will cause Ambiguous method invocation.
  * Created by akanippayyur on 10/8/16.
  */
@@ -12,6 +16,18 @@ public class FunctionOverloading {
     private static int search(Integer a) {
         return a;
     }
+
+    private static int search(Integer a, Integer b) {
+        return a + b;
+    }
+
+    private static int search(String a, String b) {
+        return Integer.parseInt(a + b);
+    }
+
+    /*private static String search(String a, String b) {
+        return "" + a + "" + b;
+    }*/
 
     private static String search(String a) {
         return a;
